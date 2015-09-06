84985ab3771dcedfc231a8b59b5266683a46ce4c
Marco Boschetti <mboschetti@itba.edu.ar>
2015-09-06 15:37:05 -0300

    Working on shell

--- client.c
+++ client.c
@@ -1,8 +1,7 @@
 //#include "sharedFunctions.h"
 #include <stdio.h>
 #include <fcntl.h>
-#include <string.h>
-#include <stdlib.h>
+
 #include "sharedFunctions.h"
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -13,36 +12,39 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+
 #define INPUT_SIZE 1024
+#define COM_SIZE 2
+
+int convertArg(char ** args, char * argTypes, int cant);
 void parse(char* buff);
+void loadCommands();
+void cexit();
+
+struct command
+{
+        char * name;
+        void (* function) ();
+        unsigned char * args;
+        int argsCant;
+        char* desc;
+};
+static struct command commands[COM_SIZE] = {};
+
 int main (int argc, char const *argv[]) {
 
-/*
-printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
-printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
-printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
-printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
-printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
-printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
-*/
-
+	loadCommands();
 	connect();
 	printf(ANSI_COLOR_GREEN"CLIENTE CONECTADO, abriendo shell..."ANSI_COLOR_RESET"\n");
 
-	char* input=malloc(INPUT_SIZE);
+	char input[INPUT_SIZE];
 	int n;
 	
 	while(1){
-		printf(ANSI_COLOR_GREEN":) " ANSI_COLOR_CYAN  " :" ANSI_COLOR_RED );
+		printf(ANSI_COLOR_GREEN":):" ANSI_COLOR_RED );
 		fflush(stdout);
-
-		fgets(input,INPUT_SIZE, stdin);
-		char* aux=input;
-		while(*(aux++));
-		aux-=2;
-		*(aux)=0;
-
-
+		gets(input);
+		printf("Logged\n");
 		parse(input);
 		
 	}
@@ -52,23 +54,47 @@ printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
 	return 0;
 }
 
-const char * accepted_commands[] = {
-    "exit",
-    "getMovieList",
-};
-int accepted_commands_size=2;
+void loadCommands(){
+	unsigned char clearArgs[] = {};
+    commands[0].name = "getMovieList";
+    commands[0].function = &getMovieList;
+    commands[0].args = clearArgs;
+    commands[0].argsCant = 0;
+    commands[0].desc = "Clears the screen, restoring it to its purest state";
+
+	unsigned char echoArgs[] = {};
+    commands[1].name = "exit";
+    commands[1].function = &cexit;
+    commands[1].args = echoArgs;
+    commands[1].argsCant = 2;
+    commands[1].desc = "Prints a given string with the provided format.";
+}
 
 void parse(char* buff){
-
-	int i;
-	for(i=0;i<accepted_commands_size;i++){
-		if(!strcmp(accepted_commands[i],buff)){
-			if(i==0){
-				printf("Chau =D"ANSI_COLOR_RESET" \n");
-				exit(0);
-			}else if(i==1){
-				getMovieList();
-			}
+int index;
+for(index = 0; index<COM_SIZE ; index++){
+if(!strcmp(buff, commands[index].name)){
+//	convertArg(args, commands[index].args, commands[index].argsCant);
+	
+	switch(commands[index].argsCant){
+	case 0:
+		commands[index].function();
+		break;
+	case 1:
+//		commands[index].function(args[1]);
+		break;
+	case 2:
+//		commands[index].function(args[1], args[2]);
+		break;
+	case 3:
+//		commands[index].function(args[1], args[2], args[3]);
+		break;
+		}
 		}
 	}
+}
+
+void cexit(){
+	printf("Saliendo!" ANSI_COLOR_RESET "\n");
+	exit(0);
 }
