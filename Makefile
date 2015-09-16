bb640378c1729dda74a7184516540c4a06c4dfdf
Juan Cruz Lepore <juancruz.lepore@hotmail.com>
2015-09-15 23:05:53 -0300

    bastante de F&S

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-all: client.c APICli.c communicationFifo.c server.c APIServer.c
-	gcc communicationFifo.c APICli.c client.c -o client
-	gcc communicationFifo.c APIServer.c server.c sqlite/sqlite3.c sqlLib.c -o server -lpthread -ldl
+all: client.c APICli.c communicationFilesAndSignals.c server.c APIServer.c
+	gcc communicationFilesAndSignals.c APICli.c client.c -o client
+	gcc communicationFilesAndSignals.c APIServer.c server.c sqlite/sqlite3.c sqlLib.c -o server -lpthread -ldl
--- communicationFilesAndSignals.c
+++ communicationFilesAndSignals.c
@@ -9,24 +9,47 @@
 #include "./datagram.h"
 
 int leftStringMatch(char * begin, char * string);
+void mypause(int sign);
 
-int is_server, reading = 0, fd;
+int is_server, reading = 0, fd, aux;
 DIR *dir;
 struct dirent *ent;	
+sigset_t mask, oldmask;
+char auxS[32];
 
 initChannel(int bool_server){
 	is_server = bool_server;
 	if(is_server){
+		signal(SIGUSR1, mypause);
+		fd = open("/tmp/server_pid", O_CREAT | O_WRONLY);
+		aux = getpid();
+		printf("mi pid es: %i\n",  aux);
+		write(fd, &aux, sizeof(int));
 		dir = opendir ("/tmp/");
 	}
 }
 
 sendData(Connection * connection, Datagram * params){
+	fd = open("/tmp/server_pid", O_RDONLY);
+	read(fd, auxS, sizeof(int));
+	aux = *((int*)auxS);
+	printf("server pid es: %i\n",  aux);
+	kill(aux, SIGUSR1);
 	
 }
 
 receiveData(Connection * sender, Datagram * buffer){
-	if(is_server){
+
+	sigemptyset (&mask);
+	sigaddset (&mask, SIGUSR1);
+	
+	sigprocmask (SIG_BLOCK, &mask, &oldmask);
+	sleep(20);
+	printf("termino el sleep\n");
+	sigsuspend(&oldmask);
+	printf("libre!\n");
+	while(1);
+	/*if(is_server){
 		sigprocmask();
 		if(reading){
 			while ((ent = readdir (dir)) != NULL && !leftStringMatch("request", ent->d_name));
@@ -45,10 +68,14 @@ receiveData(Connection * sender, Datagram * buffer){
 		}else{
 			sigsuspend();
 		}
-	}
+	}*/
 }
 leftStringMatch(char * begin, char * string){
 	int match = 1;
 	while (*begin!=0 && match) if(*(begin++)!=*(string++)) match = 0;
 	return match;
+}
+
+void mypause(int sign){
+	printf("entro a la señal\n");
 }
